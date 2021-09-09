da1b84ccd74c07537861a02c6b2fa9c784f6ca39
Felipe <felipe.portillo.delgado@gmail.com>
2021-09-09 13:38:49 -0300

    server static folder public

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', (request, response) => {
   const page = `${__dirname}/views/index.html`;
   response.sendFile(page);
